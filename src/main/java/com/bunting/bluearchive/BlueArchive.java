ae20320ec107601b54c54fbb9735821711735760
DESKTOP-LTULUJU\ASUS <chj990707@postech.ac.kr>
2023-06-27 21:32:33 +0900

    first commit

---
+++ src/main/java/com/bunting/bluearchive/BlueArchive.java
@@ -0,0 +1,43 @@
+package com.bunting.bluearchive;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BlueArchive.MODID)
+public class BlueArchive
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "bluearchive";
+
+    public BlueArchive()
+    {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
---
+++ src/main/java/com/bunting/bluearchive/entity/HoshinoEntity.java
@@ -0,0 +1,13 @@
+package com.bunting.bluearchive.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.level.Level;
+
+public class HoshinoEntity extends PathfinderMob
+{
+    public HoshinoEntity(EntityType<? extends PathfinderMob> p_21683_, Level p_21684_)
+    {
+        super(p_21683_, p_21684_);
+    }
+}
